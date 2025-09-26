c91e083157e019b118ae43e7acc6c30434884eb1
ootakazuhiko <ootakazuhiko@gmail.com>
2025-09-26 15:51:02 +0900

    test: harden repro writer escaping

--- src/testing/repro-writer.ts
+++ src/testing/repro-writer.ts
@@ -1,7 +1,25 @@
 import { writeFile, mkdir } from 'node:fs/promises';
 
+const CHAR_ESCAPE_MAP: Record<string, string> = {
+  "'": "\\'",
+  "\\": "\\\\",
+  "\n": "\\n",
+  "\r": "\\r",
+  "\t": "\\t",
+  "\f": "\\f",
+  "\b": "\\b",
+  "\u2028": "\\u2028",
+  "\u2029": "\\u2029",
+  "<": "\\u003C",
+  ">": "\\u003E",
+  "/": "\\u002F",
+  "\0": "\\0",
+};
+
+const ESCAPE_REGEX = /['\\\n\r\t\f\b\u2028\u2029<>/\0]/g;
+
 function escapeForSingleQuotedString(str: string): string {
-  return str.replace(/\\/g, '\\\\').replace(/'/g, "\\'");
+  return str.replace(ESCAPE_REGEX, (char) => CHAR_ESCAPE_MAP[char] ?? char);
 }
 
 function sanitizeFilename(str: string): string {
